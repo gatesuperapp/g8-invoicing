c5e235a5a57b98e6dfe8a5f2d8c5f31e68a498d7
maluhialoha <maluhia@ikmail.com>
2024-11-03 19:47:45 +0100

    increment version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.a4a.g8invoicing"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20
+        versionCode = 21
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
