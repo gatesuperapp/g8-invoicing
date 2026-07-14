3fb11f45a19a2956ec4bb89cea6a272d17b72325
maluhia <maluhia@ikmail.com>
2026-07-14 23:27:48 +0200

    Bump to 1.7.3 (versionCode 56)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 26
 
         targetSdk = 36
-        versionCode = 55
-        versionName = "1.7.2"
+        versionCode = 56
+        versionName = "1.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
