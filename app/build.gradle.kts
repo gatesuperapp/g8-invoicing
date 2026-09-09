a69f54902c5f40f20d71a6618747d45d2d4c70e9
maluhia <maluhia@ikmail.com>
2026-09-09 19:17:27 +0200

    Bump to versionCode 63 + 1.9.1 changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         minSdk = 26
 
         targetSdk = 36
-        versionCode = 62
-        versionName = "1.9"
+        versionCode = 63
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
